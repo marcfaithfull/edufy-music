92358c1f1e3605eb17b6522e269c9d4f54f68254
marcfaithfull <marcfaithfull@gmail.com>
2025-10-23 10:13:41 +0200

    Initial commit

---
+++ src/main/java/org/example/microservicemusic/MicroserviceMusicApplication.java
@@ -0,0 +1,13 @@
+package org.example.microservicemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceMusicApplication.class, args);
+    }
+
+}
